7c6fe9795ad289a290b9e1820285d35c0e9dd990
kJeriar <karla.jeria@gmail.com>
2017-06-22 10:01:37 -0400

    aparece el mapa, y ruta

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,35 +1,82 @@
+function initMap() {
+    
+    var map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 12,
+        center: { lat: -33.5157491, lng: -70.600865 },
+        mapTypeControl: false,
+        zoomControl: false,
+        streetViewControl: false
+    });
 
-function initMap(){
-	var map = new google.maps.Map(document.getElementById("map"),{
-	zoom: 5,
-	center:{lat: -9.1191427, lng: -77.0349046},
-	mapTypeControl:false,
-	zoomControl: false,
-	streetViewControl:false
-});
+	//autocompletado
+    var origen = document.getElementById("origen");
+    var autocomplete = new google.maps.places.Autocomplete(origen);
+    autocomplete.bindTo("bounds", map);
 
-function buscar(){
-	if (navigator.geolocation) {
-		vavigator.geolocation.getCurrentPosition(funcionExito, funcionError);
-	}
-}
+    var destino = document.getElementById("destino");
+    var autocomplete = new google.maps.places.Autocomplete(destino);
+    autocomplete.bindTo("bounds", map);
 
-document.getElementById("encuentrame").addEventListener("click", buscar);
+    // Evento para encontrar la localización actual 
+    //document.getElementById("encuentrame").addEventListener("click", buscar);
 
-var latitud,longitud;
+    function buscar() {
+        if (navigator.geolocation) {
+            //  ubicacion actual
+            navigator.geolocation.getCurrentPosition(funcionExito, funcionError);
+        }
+    }
 
-var funcionExito = function(posicion){
-	latitud = posicion.coords.latitude;
-	longitud = posicion.coords.longitude;
+     window.addEventListener("load", buscar);
 
-	var miUbicacion = new google.maps.Marker({
-		position:{lat:latitud, lng:longitud},
-		animation: google.maps.Animation.DROP,map:map
-	});
-	map.setZoom(17);
-	map.setCenter({lat:latitud, lng:longitud});
-}
+    var latitud, longitud;
+    var funcionExito = function(posicion) {
+    latitud = posicion.coords.latitude;
+    longitud = posicion.coords.longitude;
 
-var funcionError=function(error){
-	alert("tenemos un problema al encontrar tu ubicacion");
-}
+          var miUbicacion = new google.maps.Marker({
+              position: { lat: latitud, lng: longitud },
+              animation: google.maps.Animation.DROP,
+              map: map
+          });
+         map.setZoom(17);
+         map.setCenter({ lat: latitud, lng: longitud });
+     }
+
+    //no encuentra la ubicacion "error"
+    var funcionError = function(error) {
+        alert("Tenemos un problema con encontrar tu ubicación");
+    }
+
+    // trazar ruta
+    var directionsService = new google.maps.DirectionsService();
+    // suppressMarkers: true -> suprimir los iconos por defecto
+    var directionsDisplay = new google.maps.DirectionsRenderer({ suppressMarkers: false });
+
+    document.getElementById("origen").addEventListener("change", onChangeHandler);
+    document.getElementById("destino").addEventListener("change", onChangeHandler);
+
+    function calculateAndDisplayRoute(directionsService, directionsDisplay) {
+        directionsService.route({
+            origin: document.getElementById("origen").value,
+            destination: document.getElementById("destino").value,
+            travelMode: "DRIVING"
+        }, function(response, status) {
+            if (status === "OK") {
+                directionsDisplay.setDirections(response);
+                var leg = response.routes[0].legs[0];
+            } else {
+                window.alert("Directions request failed due to" + status);
+            }
+        });
+    }
+
+    directionsDisplay.setMap(map);
+
+    var onChangeHandler = function() {
+        calculateAndDisplayRoute(directionsService, directionsDisplay);
+    };
+
+    document.getElementById("boton-ruta").addEventListener("click", onChangeHandler);
+
+};
